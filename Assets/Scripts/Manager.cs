4d7af9b942aa7ba70ce9e58d0567ab543bedde33
cleeuh <clee48@hawaii.edu>
2023-10-05 20:46:48 -1000

    Completed Event System
    Complete event transition script.  Still need to make potential transitional scripts to enable and disable particles and such....

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using System;
 
 public class Manager : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform cameraGimble;
     public GameObject scenesContainer;
+    public GameObject transition;
     public Vector3 outOfBridgePosition;
 
+    private bool isTransitioning;
+    private int currentScene;
     private bool cameraStateFlag;
     private Vector3 defaultCameraPosition;
     //private Quaternion defaultCameraRotation;
@@ -24,8 +29,11 @@ public class Manager : MonoBehaviour
         //defaultCameraRotation = cameraGimble.transform.rotation;
         for (int i = 0; i < scenesContainer.transform.childCount; i++)
         {
+            scenesContainer.transform.GetChild(i).gameObject.SetActive(false);
             scenes.Add(scenesContainer.transform.GetChild(i).gameObject);
         }
+        scenes[0].SetActive(true);
+        transition.SetActive(false);
     }
     void Start()
     {
@@ -36,6 +44,44 @@ public class Manager : MonoBehaviour
     void Update()
     {
         cameraGimble.position = Vector3.Lerp(cameraGimble.position, targetCameraPosition, Time.deltaTime * 1.5f);
+    }
+
+    private void IncrementScene(int amount)
+    {
+        currentScene += amount;
+        if (currentScene >= scenes.Count)
+        {
+            currentScene = 0;
+        }
+        else if (currentScene < 0)
+        {
+            currentScene = scenes.Count - 1;
+        }
+    }
+
+    private async void ExecuteTransitionAnimation(Action callBack)
+    {
+        transition.SetActive(true);
+        await Task.Delay(5000);
+        transition.SetActive(false);
+        callBack();
+    }
+    public void ChangeEvent()
+    {
+        if (isTransitioning)
+            return;
+
+        isTransitioning = true;
+
+        void EndScene()
+        {
+            scenes[currentScene].SetActive(true);
+            isTransitioning = false;
+        }
+
+        scenes[currentScene].SetActive(false);
+        IncrementScene(1);
+        ExecuteTransitionAnimation(EndScene);
     }
 
     public void ToggleCameraPosition()
