ffd1070c1c9625be92655df965a4177039373a5c
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2023-10-07 23:44:42 -1000

    Add ship movement script and first x-wing flyby

---
+++ Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private float[] transitionTimes;
+
+    private Transform currentWaypoint;
+    private Transform nextWaypoint;
+    private int index = 0;
+
+    void Start()
+    {
+        currentWaypoint = waypoints[0];
+        nextWaypoint = waypoints[1];
+        StartCoroutine(TransitionToWaypoint());
+    }
+
+    private IEnumerator TransitionToWaypoint()
+    {
+        float transitionStartTime = Time.time;
+
+        while (Time.time - transitionStartTime < transitionTimes[index])
+        {
+            float time = (Time.time - transitionStartTime) / transitionTimes[index];
+            transform.localPosition = Vector3.Lerp(currentWaypoint.localPosition, nextWaypoint.localPosition, time);
+            transform.localRotation = Quaternion.Slerp(currentWaypoint.localRotation, nextWaypoint.localRotation, time);
+            yield return null;
+        }
+
+        transform.localPosition = nextWaypoint.localPosition;
+        transform.localRotation = nextWaypoint.localRotation;
+
+        index++;
+
+        if (index >= waypoints.Length - 1)
+        {
+            gameObject.SetActive(false);
+            yield break;
+        }
+
+        currentWaypoint = waypoints[index];
+        nextWaypoint = waypoints[index + 1];
+
+        StartCoroutine(TransitionToWaypoint());
+    }
+}
