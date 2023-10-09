fe3642db66519b4c0277711a4dd800b7ca3ca43f
Giorgio <tran.giorgio@gmail.com>
2023-10-08 22:34:50 -1000

    Add star destroyer autoaim laser

---
+++ Assets/Scripts/StarDestroyerShoot.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+public class StarDestroyerShoot : MonoBehaviour
+{
+    private ParticleSystem emitter;
+    public AudioSource sound;
+    [SerializeField]
+    public GameObject target;
+    // Start is called before the first frame update
+    // void Start()
+    // {
+
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     emitter = GetComponent<ParticleSystem>();
+    //     // Shoot();
+    // }
+
+    public void Shoot()
+    {
+        emitter = GetComponent<ParticleSystem>();
+        sound.Play();
+        // Calculate direction to the target
+        Vector3 targetDirection = target.transform.position - transform.position;
+        // rotate emitter to face the target
+        emitter.transform.rotation = Quaternion.LookRotation(targetDirection);
+        emitter.Emit(1);
+        // Task.Delay(200);
+        // emitter.Emit(1);
+
+    }
+}
