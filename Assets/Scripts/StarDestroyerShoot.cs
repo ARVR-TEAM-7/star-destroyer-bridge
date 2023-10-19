75088a5b43af407613453a2973c306a0f60c581a
Giorgio <tran.giorgio@gmail.com>
2023-10-18 15:01:58 -1000

    Update shooting to burst

--- Assets/Scripts/StarDestroyerShoot.cs
+++ Assets/Scripts/StarDestroyerShoot.cs
@@ -9,6 +9,7 @@ public class StarDestroyerShoot : MonoBehaviour
     [SerializeField]
     public GameObject target;
     public bool isShooting = false;
+    public int burst = 0;
     // Start is called before the first frame update
     // void Start()
     // {
@@ -24,8 +25,7 @@ public class StarDestroyerShoot : MonoBehaviour
 
     public async void Shoot()
     {
-      isShooting = !isShooting;
-      while(isShooting == true) {
+      for (int i = 0; i < 3; i++) {
         emitter = GetComponent<ParticleSystem>();
         sound.Play();
         // Calculate direction to the target
@@ -34,8 +34,8 @@ public class StarDestroyerShoot : MonoBehaviour
         emitter.transform.rotation = Quaternion.LookRotation(targetDirection);
         emitter.Emit(1);
         await Task.Delay(175);
+      }
         // Task.Delay(200);
         // emitter.Emit(1);
-      }
     }
 }
